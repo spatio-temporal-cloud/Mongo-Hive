2d85a7231a0149ff41c71ef6bda9c9bfbdeac43c
zengmingyu <346670414@qq.com>
2014-03-10 10:33:02 +0800

    update files uadded

--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
-tar -zxvf update.tar.gz
+#!/bin/bash
+
+rm -rf update/*
+
+rm create_table.hql
+rm deployment.py
+#rm cronjob.sh
+#rm installer.sh
+#rm synchronization.jar
+rm old_data.sh
 
 rm support_dashboard1.hql.template
 rm support_dashboard2.hql.template
@@ -7,20 +16,9 @@ rm marketing_dashboard2.hql.template
 rm marketing_dashboard3.hql.template
 rm marketing_dashboard4.hql.template
 
-rm support_dashboard1.hql
-rm support_dashboard2.hql
-rm marketing_dashboard1.hql
-rm marketing_dashboard2.hql
-rm marketing_dashboard3.hql
-rm marketing_dashboard4.hql
 
-rm deployment.py
-rm cronjob.sh
+tar -zxvf update.tar.gz
 
-rm lock
-rm cronjob.log
-
-touch cronjob.log
 
 cp ./update/support_dashboard1.hql.template ./
 cp ./update/support_dashboard2.hql.template ./
@@ -30,10 +28,21 @@ cp ./update/marketing_dashboard3.hql.template ./
 cp ./update/marketing_dashboard4.hql.template ./
 
 cp ./update/deployment.py ./
-cp ./update/cronjob.sh ./
+#cp ./update/cronjob.sh ./
+#cp ./update/synchronization.jar ./
+cp ./update/old_data.sh ./
+cp ./update/create_table.hql ./
+#cp ./update/installer.sh ./
 
-rm update.tar.gz
-rm -rf update
+chmod a+x deployment.py 
+#chmod a+x cronjob.sh
+#chmod a+x synchronization.jar
+chmod a+x old_data.sh
+#chmod a+x installer.sh
+
+hive -f create_table.hql >> cronjob.log
+nohup ./old_data.sh >> cronjob.log 2>&1 &
+#./old_data.sh >> old_data.log 2>&1 &
 
 
 
